b8be75a829f0c4137a78af33f0f6d0d11b0c8fac
Sentrilio <dominik.kowalski@protonmail.com>
2019-05-01 12:18:52 +0200

    initial commit

---
+++ desktop/src/com/domkow/kloshard/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.domkow.kloshard.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.domkow.kloshard.Kloshard;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Kloshard(), config);
+	}
+}
