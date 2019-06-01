5d08af519de0c14d23a597d1f131af2936892614
Sentrilio <dominik.kowalski@protonmail.com>
2019-06-01 17:49:39 +0200

    working creation and login via e-mail

--- core/src/com/domkow/kloshard/Screens/SplashScreen.java
+++ core/src/com/domkow/kloshard/Screens/SplashScreen.java
@@ -18,13 +18,32 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.domkow.kloshard.KloshardGame;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+import mk.gdx.firebase.GdxFIRAnalytics;
+import mk.gdx.firebase.GdxFIRApp;
+import mk.gdx.firebase.GdxFIRAuth;
+import mk.gdx.firebase.GdxFIRGoogleAuth;
+import mk.gdx.firebase.GdxFIRLogger;
+import mk.gdx.firebase.analytics.AnalyticsEvent;
+import mk.gdx.firebase.analytics.AnalyticsParam;
+import mk.gdx.firebase.auth.GdxFirebaseUser;
+import mk.gdx.firebase.auth.UserInfo;
+import mk.gdx.firebase.callbacks.AuthCallback;
+import mk.gdx.firebase.functional.Consumer;
+
 public class SplashScreen implements Screen {
     private Viewport viewport;
     private Stage stage;
     private Game game;
     private AssetManager manager;
+    private GdxFIRAuth auth;
 
     public SplashScreen(Game game) {
+
+        this.auth = GdxFIRAuth.instance();
         this.manager = ((KloshardGame) game).manager;
         this.game = game;
         viewport = new FitViewport(KloshardGame.V_WIDTH, KloshardGame.V_HEIGHT, new OrthographicCamera());
@@ -42,6 +61,35 @@ public class SplashScreen implements Screen {
         table.add(playAgainLabel).expandX().padTop(10f);
 
         stage.addActor(table);
+
+        String email = "domkow40002@gmail.com";
+        String passwordString = "Julian96@";
+        char[] psswd = passwordString.toCharArray();
+
+        auth.createUserWithEmailAndPassword(email, psswd, new AuthCallback() {
+            @Override
+            public void onSuccess(GdxFirebaseUser user) {
+                Gdx.app.log("Creation result","success");
+            }
+
+            @Override
+            public void onFail(Exception e) {
+                Gdx.app.log("Exception",e.getMessage());
+                Gdx.app.log("Creation result","fail");
+
+            }
+        });
+        auth.signInWithEmailAndPassword(email, psswd, new AuthCallback() {
+            @Override
+            public void onSuccess(GdxFirebaseUser user) {
+                Gdx.app.log("Login result","success");
+            }
+            @Override
+            public void onFail(Exception e) {
+                Gdx.app.log("Login result","fail");
+            }
+        });
+
     }
 
     @Override
