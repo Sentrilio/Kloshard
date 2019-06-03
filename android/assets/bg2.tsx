bd2542428dad8ed8216a84d96d555953898e7097
Sentrilio <dominik.kowalski@protonmail.com>
2019-06-03 03:24:24 +0200

    changed map's background

---
+++ android/assets/bg2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="bg2" tilewidth="70" tileheight="70" spacing="1" margin="1" tilecount="9" columns="3">
+ <image source="textures/bg2.png" width="256" height="256"/>
+</tileset>
